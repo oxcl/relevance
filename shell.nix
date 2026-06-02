a791651a0e8ea78cb94d1d3b271d30ad666788da
oxcl <oxcl@proton.me>
2026-06-02 17:08:26 +0400

    fix: simplify fdroid update, use env var for GH_TOKEN

--- shell.nix
+++ shell.nix
@@ -57,10 +57,15 @@ pkgs.mkShell {
     export RELEVANCE_TOOLS_DIR="''${RELEVANCE_TOOLS_DIR:-$PWD/.tools}"
     mkdir -p "$RELEVANCE_TOOLS_DIR"
 
-    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:''${LD_LIBRARY_PATH:-}"
 
     export ANDROID_HOME="${androidComposition.androidsdk}/libexec/android-sdk"
     export PATH="$ANDROID_HOME/build-tools/${buildToolsVersion}:$PATH"
+
+    # GitHub CLI token
+    if [ -n "$GITHUB_TOKEN" ]; then
+      export GH_TOKEN="$GITHUB_TOKEN"
+    fi
 
     if [ ! -d "$PWD/.venv" ]; then
       ${python}/bin/python3 -m venv "$PWD/.venv"
