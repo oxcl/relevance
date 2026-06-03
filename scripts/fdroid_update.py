7a8bf595842d083f6dcec1ea531dfc85d45a803c
oxcl <oxcl@proton.me>
2026-06-03 08:02:30 +0400

    fix: patch NoOverwriteDict class via instance type

--- scripts/fdroid_update.py
+++ scripts/fdroid_update.py
@@ -3,31 +3,31 @@
 import os
 import sys
 
-# Monkey-patch androguard BEFORE importing fdroidserver
 try:
     import androguard.core.apk as apk_mod
 
-    # Patch APK.__init__ to convert _v2_blocks from dict to list
+    # Patch __init__ to find and fix NoOverwriteDict on the instance
     _orig_init = apk_mod.APK.__init__
 
     def _patched_init(self, *args, **kwargs):
         _orig_init(self, *args, **kwargs)
-        # If _v2_blocks is a dict (older androguard), convert to list
-        if isinstance(self._v2_blocks, dict):
-            self._v2_blocks = list(self._v2_blocks.values())
+        blocks = self._v2_blocks
+        # Add append method to the class if missing
+        cls = type(blocks)
+        if not hasattr(cls, 'append'):
+            def _append(self_dict, item):
+                key = item[0] if isinstance(item, tuple) else getattr(item, 'id', len(self_dict))
+                self_dict[key] = item
+            cls.append = _append
 
     apk_mod.APK.__init__ = _patched_init
 except Exception as e:
     print(f"Warning: Could not patch androguard: {e}", file=sys.stderr)
 
-# Change to fdroid directory if it exists
 fdroid_dir = os.path.join(os.getcwd(), 'fdroid')
 if os.path.exists(fdroid_dir):
     os.chdir(fdroid_dir)
 
-# Set up argv for fdroidserver
 sys.argv = ['fdroid update', '--create-metadata']
-
-# Import and run fdroidserver update directly
 import fdroidserver.update
 fdroidserver.update.main()
