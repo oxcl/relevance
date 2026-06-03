0c53c74f599adf3f47e49514bbcb85616d453e5e
oxcl <oxcl@proton.me>
2026-06-03 07:56:59 +0400

    fix: convert _v2_blocks from dict to list after APK init

--- scripts/fdroid_update.py
+++ scripts/fdroid_update.py
@@ -1,57 +1,22 @@
 #!/usr/bin/env python3
-"""Monkey-patch androguard NoOverwriteDict bug, then run fdroid update."""
+"""Monkey-patch androguard _v2_blocks bug, then run fdroid update."""
 import os
 import sys
 
-# Find and patch NoOverwriteDict in any androguard module
+# Monkey-patch androguard BEFORE importing fdroidserver
 try:
-    import androguard
-    
-    # Search all androguard submodules for NoOverwriteDict
-    _orig = None
-    _module = None
-    
-    for mod_name in ['androguard.core.apk', 'androguard.core.dex', 'androguard.core.bytecode', 'androguard.util']:
-        try:
-            mod = __import__(mod_name, fromlist=[''])
-            if hasattr(mod, 'NoOverwriteDict'):
-                _orig = mod.NoOverwriteDict
-                _module = mod
-                break
-        except ImportError:
-            pass
-    
-    if _orig is None:
-        # Search all attributes of androguard.core.apk
-        import androguard.core.apk as apk_mod
-        for name in dir(apk_mod):
-            obj = getattr(apk_mod, name)
-            if isinstance(obj, type) and 'NoOverwrite' in name:
-                _orig = obj
-                _module = apk_mod
-                break
-    
-    if _orig is not None:
-        # Add append method directly to the class
-        def _append(self, item):
-            key = item[0] if isinstance(item, tuple) else getattr(item, 'id', len(self))
-            self[key] = item
-        
-        _orig.append = _append
-        
-        # Also patch APK.__init__ to fix existing instances
-        import androguard.core.apk as apk_mod
-        _orig_init = apk_mod.APK.__init__
-        
-        def _patched_init(self, *args, **kwargs):
-            _orig_init(self, *args, **kwargs)
-            if hasattr(self, '_v2_blocks') and not hasattr(self._v2_blocks, 'append'):
-                self._v2_blocks.__class__ = _orig
-        
-        apk_mod.APK.__init__ = _patched_init
-    else:
-        print("Warning: NoOverwriteDict not found in androguard", file=sys.stderr)
-        
+    import androguard.core.apk as apk_mod
+
+    # Patch APK.__init__ to convert _v2_blocks from dict to list
+    _orig_init = apk_mod.APK.__init__
+
+    def _patched_init(self, *args, **kwargs):
+        _orig_init(self, *args, **kwargs)
+        # If _v2_blocks is a dict (older androguard), convert to list
+        if isinstance(self._v2_blocks, dict):
+            self._v2_blocks = list(self._v2_blocks.values())
+
+    apk_mod.APK.__init__ = _patched_init
 except Exception as e:
     print(f"Warning: Could not patch androguard: {e}", file=sys.stderr)
 
